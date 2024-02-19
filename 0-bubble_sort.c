17836847586c2e1d3aade386db04aa7145dfb001
preciousanyi <jkpresh@gmail.com>
2024-02-19 16:50:27 +0100

    Modified file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,17 +7,16 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, len;
+	size_t i, j;
 	int temp;
 	int swapped;
 
-	len = size;
 	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < len - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-		for (j = 0; j < len - 1 - i; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
